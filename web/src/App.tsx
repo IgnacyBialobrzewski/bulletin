0e667d5a9c5952b7b734ffbae6a0ad4b4c109361
Ignacy Bialobrzewski <ignacybialobrzewski99@gmail.com>
2024-10-22 16:58:04 +0200

    initial commit

---
+++ web/src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css"
+
+export default function App() {
+    return (
+        <main>
+            <h1 className="text-red-500">Hello nigga</h1>
+            <h2>Hello nigga</h2>
+        </main>
+    )
+}
