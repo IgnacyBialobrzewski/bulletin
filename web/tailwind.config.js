1ed35f2fb4eadac8fd19b635dafeaecd5be03702
IgnacyBialobrzewski <ignacy.bialobrzewski@zseitolsztyn.onmicrosoft.com>
2024-11-26 12:34:27 +0100

    gancI

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,9 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            'menu-image':  "url('menu.svg')"
+        },
     },
     plugins: [],
 }
