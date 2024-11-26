9884c886050fa96fab9150182715537c03fe980b
Adrian Kulik <Adriankulik311@gmail.com>
2024-11-26 18:26:41 +0100

    Menu now opens and closes upon pressing it.

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,14 +1,23 @@
-import menu from "../assets/menu.svg"
+import { useState } from 'react';
+import menu from "../assets/menu.svg";
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <nav className="bg-blue-500 p-4">
-            <img className="text-white h-[40px] mb-1" src={menu}></img>
-            <ul className="text-white text-lg flex flex-col flex-wrap gap-1">
+            <div className="flex items-center justify-between">
+                <img className="text-white h-[40px] mb-1 cursor-pointer" src={menu} alt="Menu" onClick={toggleMenu}/>
+            </div>
+            <ul className={`text-white text-lg gap-1 mt-2 ${isOpen ? 'block' : 'hidden'}`}>
                 <li><a href="" className="no-underline hover:underline ml-2">Contact</a></li>
                 <li><a href="" className="no-underline hover:underline ml-2">Info</a></li>
                 <li><a href="" className="no-underline hover:underline ml-2">Help</a></li>
             </ul>
         </nav>
-    )
+    );
 }
